fdddc0366f280929158685cb7ede10412933e7c0
Thari Zephaniah <t-zephaniah@mercari.com>
2024-06-23 22:08:44 +0530

    adds env

--- config/settings/development.py
+++ config/settings/development.py
@@ -2,7 +2,7 @@ from .base import *
 import os
 
 DEBUG = True
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS').split(' ')
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', "localhost 127.0.0.1").split(' ')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
